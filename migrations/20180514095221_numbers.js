10cb5e103fb27d5a2589e0f166103d8797e4e296
Landon Folkers <32934735+landonfolkers@users.noreply.github.com>
2018-05-14 10:13:45 -0600

    init commit

---
+++ migrations/20180514095221_numbers.js
@@ -0,0 +1,14 @@
+exports.up = function (knex, Promise) {
+    return knex.schema.createTable('numbers', table => {
+        table.increments('id').primary().
+        table.text('identity').references('messages.identity')
+        table.text('number1')
+        table.text('number2')
+        table.text('number3')
+        table.text('number4')
+        table.text('number5')
+    })
+}
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('numbers')
+  }
